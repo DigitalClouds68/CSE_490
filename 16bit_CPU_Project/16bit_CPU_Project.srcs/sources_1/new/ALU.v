22be91c5a2f15414d7a0e6570d1c16d47a86b11b
DigitalClouds68 <DigitalClouds68@gmail.com>
2025-04-09 23:00:20 -0400

    Update ALU.v

--- 16bit_CPU_Project/16bit_CPU_Project.srcs/sources_1/new/ALU.v
+++ 16bit_CPU_Project/16bit_CPU_Project.srcs/sources_1/new/ALU.v
@@ -10,12 +10,14 @@ module ALU (
             4'b0000: ALUOut = A + B;    // add
             4'b0001: ALUOut = A - B;    // sub
             4'b0010: ALUOut = B << A;   // sll
-            4'b0011: ALUOut = A & B;    // AND
-            4'b0100: ALUOut = A | B;    // OR
-            4'b0101: ALUOut = A ^ B;    // XOR
+            4'b0011: ALUOut = A & B;    // and
+            
+            /*Plan to implement it in the future...*/
+            4'b0100: ALUOut = A | B;    // or
+            4'b0101: ALUOut = A ^ B;    // xor
+            
             default: ALUOut = 16'h0000;
         endcase
-        // (Not sure if it's usable)Zero flag is true if result is zero
         Zero = (ALUOut == 16'h0000) ? 1'b1 : 1'b0;
     end
 endmodule
