9141d5bdd617416fee4d1285c3864562c3f27087
DigitalClouds68 <DigitalClouds68@gmail.com>
2025-04-09 23:05:06 -0400

    Update RegisterFile.v

--- 16bit_CPU_Project/16bit_CPU_Project.srcs/sources_1/new/RegisterFile.v
+++ 16bit_CPU_Project/16bit_CPU_Project.srcs/sources_1/new/RegisterFile.v
@@ -7,19 +7,34 @@ module RegisterFile (
     input  wire  [3:0]  write_reg,
     input  wire  [15:0] write_data,
     output wire  [15:0] read_data1,
-    output wire  [15:0] read_data2
+    output wire  [15:0] read_data2,
+    
+    /***********Just try************/
+    /**Follow the demo requirement**/
+    output reg  [15:0] debug_out
+    /********************************/
 );
-    /* Add sth here */
     reg [15:0] regfile [0:15];
+    
+    /*********Initialize to zero*********/
     integer k;
     always @(posedge clk or posedge reset) begin
         if (reset) begin
             for(k=0; k<16; k=k+1)
                 regfile[k] <= 16'h0000;
+            debug_out <= 16'h0000;
         end else if (RegWrite) begin
             regfile[write_reg] <= write_data;
+            debug_out         <= write_data;        
+        end else begin
+            debug_out <= debug_out;
         end
     end
+    /************************************/
+    
     assign read_data1 = regfile[read_reg1];
     assign read_data2 = regfile[read_reg2];
+    
+    /* Add */
+
 endmodule
